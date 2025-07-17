26e5989ec7571e2657d94a6e41c3e68a148626b9
208040242 <kperezs@est.utn.ac.cr>
2025-07-16 23:57:33 -0600

    promos y producto

--- Server/generated/prisma/edge.js
+++ Server/generated/prisma/edge.js
@@ -339,7 +339,7 @@ const config = {
       "value": "prisma-client-js"
     },
     "output": {
-      "value": "C:\\Users\\John\\Desktop\\Git\\MipacheLibrary\\Server\\generated\\prisma",
+      "value": "C:\\Users\\perez\\Desktop\\Aplicaciones Web\\MipacheLibrary\\Server\\generated\\prisma",
       "fromEnvVar": null
     },
     "config": {
@@ -353,7 +353,7 @@ const config = {
       }
     ],
     "previewFeatures": [],
-    "sourceFilePath": "C:\\Users\\John\\Desktop\\Git\\MipacheLibrary\\Server\\prisma\\schema.prisma",
+    "sourceFilePath": "C:\\Users\\perez\\Desktop\\Aplicaciones Web\\MipacheLibrary\\Server\\prisma\\schema.prisma",
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
