59c9191569cd560653d403578c25b02760a31af6
John_fm17 <johainer10fm@gmail.com>
2025-07-31 13:05:21 -0600

    Fix de promociones + CSS

--- appMipacheLibrary/src/app/promocion/promocion-routing-module.ts
+++ appMipacheLibrary/src/app/promocion/promocion-routing-module.ts
@@ -2,9 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PromocionIndex } from './promocion-index/promocion-index';
 import { PromocionDetail } from './promocion-detail/promocion-detail';
+import { PromocionAdmin } from './promocion-admin/promocion-admin';
+import { PromocionForm } from './promocion-form/promocion-form';
 
 const routes: Routes = [
     //promociones
+    { path:'promocion-admin', component: PromocionAdmin },
+    { path:'promocion/create', component: PromocionForm },
+    { path:'promocion/update/:id', component: PromocionForm },
     { path:'promocion',component: PromocionIndex},
     { path:'promocion/:id',component: PromocionDetail},
 ];
