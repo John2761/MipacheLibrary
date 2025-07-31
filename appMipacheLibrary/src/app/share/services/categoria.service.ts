f489b2db2f851da466944a0fa05aa66343000a8c
208040242 <kperezs@est.utn.ac.cr>
2025-07-30 22:20:47 -0600

    part 5

--- appMipacheLibrary/src/app/share/services/categoria.service.ts
+++ appMipacheLibrary/src/app/share/services/categoria.service.ts
@@ -1,27 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-<<<<<<< HEAD
-import { Observable } from 'rxjs';
-import { environment } from '../../../environments/environment.development';
 
-export interface CategoriaModel {
-  id: number;
-  nombre: string;
-}
-
-@Injectable({
-  providedIn: 'root',
-})
-export class CategoriaService {
-  private baseUrl = `${environment.apiURL}/categoria`;
-
-  constructor(private http: HttpClient) {}
-
-  get(): Observable<CategoriaModel[]> {
-    return this.http.get<CategoriaModel[]>(this.baseUrl);
-  }
-}
-=======
 import { CategoriaModel } from '../models/CategoriaModel';
 import { BaseAPI } from '../base-api';
 import { environment } from '../../../environments/environment.development';
@@ -37,4 +16,3 @@ export class CategoriaService extends BaseAPI<CategoriaModel>{
           environment.endPointCategoria);
       }
 }
->>>>>>> 72ba020929ec3235577a6a8eb92ec25848597e10
